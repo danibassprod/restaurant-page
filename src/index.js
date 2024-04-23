f7203155d45e2a892ec78cc17b9a6c5646e623b6
Daniel <cristianobajista21@gmail.com>
2024-04-23 11:57:21 -0400

    Add basic homepage

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import './reset.css';
 import './styles.css';
+import backgroundCardPic from './img/brick-wall-1834784_1280.jpg';
+
+const topCard = document.querySelector('.top');
+const topCardImage = new Image();
+topCardImage.src = backgroundCardPic;
+topCardImage.classList.add('top-card-pic');
+topCard.appendChild(topCardImage);
