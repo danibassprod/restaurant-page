7120acf6d34b49e6d0ccc62e37fc1a10430dda9f
danibassprod <cristianobajista21@gmail.com>
2024-04-27 21:12:41 -0400

    Tabs render their respective module on click

--- src/index.js
+++ src/index.js
@@ -4,8 +4,56 @@ import homepage from './homepage.js'
 import menu from './menu.js'
 import contact from './contact.js'
 
-// homepage();
+homepage();
 
-// menu();
+const render = (function(){
 
-contact();
+    //renders home on click
+
+    const homeTab = document.querySelector('#home');
+    homeTab.addEventListener('click', function(){
+
+        const container = document.querySelector('#container');
+        const subContainer = document.querySelector('.sub-container');
+
+        function removeSubContainer() {
+            container.removeChild(subContainer);
+            homepage();
+        }
+
+        removeSubContainer();
+    });
+    
+    //renders menu on click
+
+    const menuTab = document.querySelector('#menu');
+    menuTab.addEventListener('click', function(){
+
+        const container = document.querySelector('#container');
+        const subContainer = document.querySelector('.sub-container');
+
+        function removeSubContainer() {
+            container.removeChild(subContainer);
+            menu();
+        }
+
+        removeSubContainer();
+    });
+
+    //renders contact on click
+
+    const contactTab = document.querySelector('#contact');
+    contactTab.addEventListener('click', function(){
+
+        const container = document.querySelector('#container');
+        const subContainer = document.querySelector('.sub-container');
+
+        function removeSubContainer() {
+            container.removeChild(subContainer);
+            contact();
+        }
+
+        removeSubContainer();
+    });
+
+})();
